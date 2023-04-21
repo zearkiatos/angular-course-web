8f22f368f7c1d3888a5571d7d849d934bee47735
zearkiatos <caprilespe@outlook.com>
2023-04-21 16:54:29 -0400

    Testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -34,6 +34,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-course-web app is running!');
+    expect(compiled.querySelector('.display-2')?.textContent).toContain('Student list course');
   });
 });
